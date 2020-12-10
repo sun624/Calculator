83b2b7d5633c5d06706e7ba1c959637ccc7c5b88
sun624 <67940809+sun624@users.noreply.github.com>
2020-12-10 02:27:35 -0800

    inital version done

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        decimal num1 = 0;
+        decimal num2 = 0;
+        string Operator = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,8 +31,48 @@ namespace Calculator
 
         private void NumBtn_Click(object sender, RoutedEventArgs e)
         {
-            string name = ((Button)sender).Name;
-            MessageBox.Show(name);
+            Button btn = (Button)sender;
+
+            if (Display.Text == "0")
+                Display.Text = btn.Content.ToString();
+            else
+                Display.Text += btn.Content.ToString();
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            num1 = num2 = 0;
+            Operator = "";
+            Display.Text = "0";
+        }
+
+        private void EqualBtn_Click(object sender, RoutedEventArgs e)
+        {
+            num2 = decimal.Parse(Display.Text);
+            switch (Operator)
+            {
+                case "+":
+                    Display.Text = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    Display.Text = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    Display.Text = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    Display.Text = (num1 / num2).ToString();
+                    break;
+
+            }
+        }
+
+        private void OperatorBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Operator = btn.Content.ToString();
+            num1 = decimal.Parse(Display.Text);
+            Display.Text = "";
         }
     }
 }
